b237fbc55a1a6ab59565f4f3ab19310090a57f3d
HectorGS94 <hector.gsantoyo@alumnos.udg.mx>
2016-08-27 19:42:29 -0500

    Practica #1
    Se inicia con los primeros avances de la practica numero 1, se
    desarrolla parte de la interfaz grafica y parte de algunas clases.

---
+++ Ensamblador/src/ensamblador/Ensamblador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ensamblador;
+/**
+ *
+ * @author Hector Garcia
+ */
+public class Ensamblador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Analizador a = new  Analizador();
+        a.setVisible(true);
+    }
+    
+}
